e629d69ed729229ebd0a230b4cd38e15ca706e7e
최원빈 <wonbeen@tossbank.com>
2024-04-08 00:26:05 +0900

    전부 커밋으로  수정 & 주석 제거

--- src/cn/index.ts
+++ src/cn/index.ts
@@ -2,7 +2,6 @@ interface Classname {
   [key: string]: boolean;
 }
 
-/** 여러 개의 className을 합치는 함수 */
 export function cn(classes: Classname) {
   return Object.entries(classes)
     .filter(([, value]) => value)
